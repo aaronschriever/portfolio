92324c6b18feb7dd506cf1be62829422d013b67b
Aaron <aaron.schriever@gmail.com>
2017-10-23 23:25:16 +1030

    setup codepen iframe hosting for projects

---
+++ src/CodepenEmbed.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+
+class CodepenEmbed extends Component {
+constructor(props){
+    super(props);
+    this.state={codePen: ''};
+    
+}
+
+    componentWillMount(){
+
+  console.log(this.props);
+   let codePen = <iframe height='400' width='80%' scrolling='no' title={this.props.title} src={'//codepen.io/Arnoo/embed/'+this.props.hash+'/?height=240&theme-id=0&default-tab=js,result&embed-version=2'} frameborder='no' allowtransparency='true' allowfullscreen='true' >See the Pen <a href={'https://codepen.io/Arnoo/pen/'+ this.props.hash}>{this.props.title}</a> by Aaron (<a href='https://codepen.io/Arnoo'>@Arnoo</a>) on <a href='https://codepen.io'>CodePen</a>.
+   </iframe>
+   
+
+
+this.setState({codePen: codePen});
+    }
+
+   render(){
+return(
+    <div>
+    {this.state.codePen}
+   </div>
+    )
+}
+
+}
+export default CodepenEmbed;
--- src/Menu.js
+++ src/Menu.js
@@ -1,20 +1,48 @@
 import React, { Component } from 'react';
-const projectArray = [{title: 'Random Quote Generator', link: 'https://codepen.io/Arnoo/pen/dpZvpA', caption: 'quote generator - part of the Free Code Camp curriculum'},
-{title: 'Twitch TV Viewer', link: 'https://codepen.io/Arnoo/pen/zZvKyL', captioon: 'list view of Twitch casters pulling data from the Twitch API - part of the Free Code Camp curriculum'} ];
-class Menu extends Component {
+import CodepenEmbed from './CodepenEmbed';
+//import Codepen from 'react-codepen';
+const codepenUser = 'Arnoo';
+const projectArray = [
+    {title: 'Random Quote Generator', link: 'https://codepen.io/Arnoo/pen/dpZvpA', caption: 'Quote generator - part of the Free Code Camp curriculum', hash:'dpZvpA', tech: 'JQuery, bootstrap' },
+    {title: 'Twitch TV Viewer', link: 'https://codepen.io/Arnoo/pen/zZvKyL', caption: 'List view of Twitch casters pulling data from the Twitch API - part of the Free Code Camp curriculum', hash: 'zZvKyL', tech: 'JQuery, JS Promises, SCSS, Pug'},
+    {title: 'Wikipedia viewer', link: 'https://codepen.io/Arnoo/pen/NAQboq', caption: 'Wikipedia search app, pulling data from the Wikipedia API - part of the Free Code Camp curriculum', hash: 'NAQboq', tech: 'JQuery, SCSS, Pug, GSAP tweenmax library' },
+    {title: 'Weather App', link: 'https://codepen.io/Arnoo/pen/ORedGg', caption: 'Weather app, pulling data from Openweather API - part of the Free Code Camp curriculum', hash: 'ORedGg', tech: 'JQuery, bootstrap' }
+
+];
+
+    class Menu extends Component {
 
     constructor(props) {
         super(props);
-        this.state={projectArray: projectArray};
+        this.state = {
+            projects: []
+        };
     }
-
+  
+    componentDidMount (){
+        let projects = projectArray.map((project) =>{
+return (
+    <div key={project.id}>
+        <h2>{project.title}</h2>
+        <p>{project.caption}</p>
+        <p>Technology used: {project.tech}</p> 
+        <p><CodepenEmbed hash={project.hash} link={project.link} title={project.title}/></p>
+        
+    </div>
+)
+    });
+    this.setState({projects: projects});
+    }
+   
     render() {
         return (
             <div>
-            <h2>{this.projectArray.title}</h2>
-            <a href={this.projectArray.link}>image</a>
+            {this.state.projects}
+            
             </div>
+            
         )
     }
 }
 
+export default Menu;
