22db6c93a11eb74ad38592018653fd51a582f366
Aaron <aaron.schriever@gmail.com>
2017-10-24 21:34:30 +1030

    add calculator app, change background image.

--- src/Menu.js
+++ src/Menu.js
@@ -6,7 +6,8 @@ const projectArray = [
     {title: 'Random Quote Generator', link: 'https://codepen.io/Arnoo/pen/dpZvpA', caption: 'Quote generator - part of the Free Code Camp curriculum', hash:'dpZvpA', tech: 'JQuery, bootstrap' },
     {title: 'Twitch TV Viewer', link: 'https://codepen.io/Arnoo/pen/zZvKyL', caption: 'List view of Twitch casters pulling data from the Twitch API - part of the Free Code Camp curriculum', hash: 'zZvKyL', tech: 'JQuery, JS Promises, SCSS, Pug'},
     {title: 'Wikipedia viewer', link: 'https://codepen.io/Arnoo/pen/NAQboq', caption: 'Wikipedia search app, pulling data from the Wikipedia API - part of the Free Code Camp curriculum', hash: 'NAQboq', tech: 'JQuery, SCSS, Pug, GSAP tweenmax library' },
-    {title: 'Weather App', link: 'https://codepen.io/Arnoo/pen/ORedGg', caption: 'Weather app, pulling data from Openweather API - part of the Free Code Camp curriculum', hash: 'ORedGg', tech: 'JQuery, bootstrap' }
+    {title: 'Weather App', link: 'https://codepen.io/Arnoo/pen/ORedGg', caption: 'Weather app, pulling data from Openweather API - part of the Free Code Camp curriculum', hash: 'ORedGg', tech: 'JQuery, bootstrap' },
+    {title: 'Calculator', link: 'https://codepen.io/Arnoo/pen/ZJGZBr', caption: 'JS Calculator using React - part of the Free Code Camp curriculum', hash: 'ZJGZBr', tech: 'JS, React, SCSS'}
 
 ];
 
