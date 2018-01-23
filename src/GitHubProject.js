1d26364473994cf9dd6505595969619a8998af95
Aaron <aaron.schriever@gmail.com>
2018-01-23 21:46:28 +1030

    fix link to dartboard app

--- src/GitHubProject.js
+++ src/GitHubProject.js
@@ -5,7 +5,7 @@ function GitHubProject(props) {
       return(
       <div className="Project-container">
       <div className="Project-description">
-      <h2><a href={props.dartboard.title}>{props.dartboard.title}</a></h2>
+      <h2><a href={props.dartboard.link}>{props.dartboard.title}</a></h2>
       <p>{props.dartboard.caption}</p>
       <p>Technology used: {props.dartboard.tech}</p>        
       <img className='Project-image' src={props.img} alt={props.dartboard.alt}/>
